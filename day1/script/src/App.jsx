e171d9d42ff3bedeb99a549ca602e01f50cacaea
tofu-10 <rijanstha.lm10@gmail.com>
2025-06-23 10:35:14 +0545

    commit

---
+++ day1/script/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  <>
+    return{" "}
+    <h1 className="text-red-500 bg-amber-400">This my first react app! </h1>;
+    <p>My name is rijan shrestha
+      
+    </p>
+  </>;
+}
+
+export default App;
