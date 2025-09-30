9415944917b1fa668db39ecb6b694e9f2c54ec8c
tofu-10 <rijanstha.lm10@gmail.com>
2025-09-30 23:03:50 +0545

    trying css

--- myproject/src/Components/HeroPage/Welcome.jsx
+++ myproject/src/Components/HeroPage/Welcome.jsx
@@ -4,7 +4,7 @@ export default function WelcomeAnimation({ onFinish }) {
 useEffect(()=>{
     const timer= setTimeout(()=>{
         onFinish();
-    },100)
+    },4000)
     return ()=> clearTimeout(timer);
 
 },[onFinish])
